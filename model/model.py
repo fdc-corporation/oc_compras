bd75303f7d11cd25fbae0c2cdb16fb307fad3276
palaciosyost <palaciosyostin903@gmail.com>
2025-04-11 17:40:01 -0500

    agregando redireccion

--- model/model.py
+++ model/model.py
@@ -190,6 +190,7 @@ class OrdenCompras(models.Model):
             "target": "new",
             "context": ctx,
         }
+
     def action_post_cotizacion(self):
         self.ensure_one()
 
@@ -243,6 +244,19 @@ class OrdenCompras(models.Model):
                     "sticky": False,
                 },
             }
+
+    def ir_servicio(self):
+        ots = self.cotizacion_id.ots
+        if ots and self.env["plan.mantenimiento"].browse(ots.id).exists():
+            return {
+                "type": "ir.actions.act_window",
+                "name": "Servicio",
+                "view_mode": "form",
+                "res_model": "plan.mantenimiento",
+                "res_id": ots.id,
+                "context": {"create": False},
+            }
+
     def action_create_invoice(self):
         self.ensure_one()
         # Validar que existe cotizacion_id
