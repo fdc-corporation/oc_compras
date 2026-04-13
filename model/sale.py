aa1724fa1ead3ca4be92d9453dba438f865ac887
palaciosyost <palaciosyostin903@gmail.com>
2026-04-13 17:40:10 -0500

    cambios oc

--- model/sale.py
+++ model/sale.py
@@ -20,7 +20,7 @@ class SaleOrder (models.Model):
 
         return res
 
-
+    @api.model
     def _compute_state_factura(self):
         for record in self:
             facturas = self.env["account.move"].search([("invoice_origin", "ilike", record.name), ("move_type", "in", ["out_invoice"]), ("state", "=", "posted"), ("edi_state", "=", "sent")])
