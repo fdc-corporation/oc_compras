eb10218cac57885054c8e7cd346939eaa03aeb9f
palaciosyost <palaciosyostin903@gmail.com>
2026-04-30 15:37:16 -0500

    update model

--- model/sale.py
+++ model/sale.py
@@ -19,7 +19,18 @@ class SaleOrder (models.Model):
 
         return res
 
-
+    def copy(self, default=None):
+        default = dict(default)
+        
+        default.update(
+            {
+                "oc_id" : False,
+                "state_factura" : False,
+            }
+        )
+        return super().copy(default)
+    
+    
     def _compute_state_factura(self):
         for record in self:
             facturas = self.env["account.move"].search([("invoice_origin", "ilike", record.name), ("move_type", "in", ["out_invoice"]), ("state", "=", "posted"), ("edi_state", "=", "sent")])
